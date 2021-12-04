1b07f9395aade4deb2c83771c435242d22bcb377
@aleksei_fadeev <alex1975>
2021-12-04 15:48:38 +0300

    rehomework 7.9

--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -8,16 +8,18 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
-        for (int i = 0; i <= 65536; i++) {
-            if (((char) i == 'Ё') | ((char) i == 'ё')) {
+        for (int i = 0; i < 65536; i++) {
+            if (((char) i == 'Ё') || ((char) i == 'ё')) {
 
                 System.out.println(i + " - " + (char) i);
             }
+            if ((char) i == 'А') {
+                for (i = (int) 'А'; i <= (int) 'я'; i++) {
+                    System.out.println(i + " - " + (char) i);
+                }
+            }
         }
 
-        for (int i = (int) 'А'; i <= (int) 'я'; i++) {
-            System.out.println(i + " - " + (char) i);
-        }
 
     }
 }
