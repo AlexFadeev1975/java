a4540a4c6fba674c2d1d6d87a0c4748affce3d9b
@aleksei_fadeev <alex1975>
2021-12-18 14:18:44 +0300

    homework 10.1 done

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -3,8 +3,17 @@ public class Hospital {
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
         //TODO: напишите метод генерации массива температур пациентов
+        float [] temperatureDate = new float[patientsCount];
+        float maxTemp = 40;
+        float minTemp = 32;
+        float diffTemp = maxTemp - minTemp;
 
-        return new float[0];
+        for (int i = 0; i< patientsCount; i++) {
+
+            temperatureDate [i] = (float) (Math.random() * diffTemp + minTemp);
+        }
+
+        return temperatureDate;
     }
 
     public static String getReport(float[] temperatureData) {
@@ -12,12 +21,22 @@ public class Hospital {
         TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
             а также температуры всех пациентов.
         */
+        float sumTempetarure = 0;
+        int countPatient = 0;
+        float maxHealthTemp = 36.9f;
+        float minHealthTemp = 36.2f;
+        String arrayTostring ="";
 
-        String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
+        for (float temperatureDatum : temperatureData) {
 
-        return report;
+            arrayTostring = arrayTostring + temperatureDatum + " ";
+
+            sumTempetarure += temperatureDatum;
+            countPatient = (temperatureDatum <= maxHealthTemp && temperatureDatum >= minHealthTemp) ? countPatient + 1 : countPatient;
+        }
+
+        return "Температуры пациентов: " +  arrayTostring.trim() +
+                "\nСредняя температура: " + String.format("%.2f", (sumTempetarure / temperatureData.length)) +
+                "\nКоличество здоровых: " + countPatient;
     }
 }
