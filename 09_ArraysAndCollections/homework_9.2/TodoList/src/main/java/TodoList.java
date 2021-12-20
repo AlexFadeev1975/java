b2a58aeed84597727a3cf2ce95ad99da0cd54079
@aleksei_fadeev <alex1975>
2021-12-20 07:10:06 +0300

    homework 10.2 done

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,46 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    ArrayList<String> todoList = new ArrayList<>();
+
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+        todoList.add(todo);
+
+
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
+        if (index < todoList.size()) {
+            todoList.add(index, todo);
+        } else {
+            todoList.add(todo);
+        }
     }
+
 
     public void edit(String todo, int index) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+        if (index < todoList.size()) {
+            todoList.set(index, todo);
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+        if (index < todoList.size()) {
+            todoList.remove(index);
+        }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return new ArrayList<>();
+
+        return todoList;
     }
 
 }
