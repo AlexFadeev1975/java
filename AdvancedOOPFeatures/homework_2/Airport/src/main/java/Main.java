82c7ee56321b6ce2ee02c8759e642e5f7fdbad39
@aleksei_fadeev <alex1975>
2022-01-29 18:15:10 +0300

    homework 13.8 done

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,37 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import com.skillbox.airport.Terminal;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(Airport.getInstance());
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
+        long currentTime = System.currentTimeMillis();
+        long timePlusTwoHours = currentTime + 7200000l;
+        Date timeNow = new Date(currentTime);
+        Date timePlus = new Date(timePlusTwoHours);
+        List<Flight> flight = new ArrayList<>();
 
+        for (Terminal item : airport.getTerminals()) {
+
+            flight.addAll(item.getFlights());
+        }
+
+        return flight.stream()
+                .filter(i -> i.getType() == Flight.Type.DEPARTURE)
+                .filter(x -> x.getDate().before(timePlus))
+                .filter(y -> y.getDate().after(timeNow))
+                .collect(Collectors.toList());
+
+    }
 }
--- AdvancedOOPFeatures/homework_2/Employees/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Employees/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -13,6 +16,15 @@ public class Main {
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
         //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
         // кто пришёл в году, указанном в переменной year
-        return null;
+
+        Date date = new Date((year - 1899), Calendar.JANUARY, 1);
+        Date date1 = new Date((year - 1901), Calendar.DECEMBER, 31);
+
+        return staff.stream().filter(e -> e.getWorkStart().before(date)).filter(m -> m.getWorkStart().after(date1)).
+                max(Comparator.comparing(Employee::getSalary)).get();
+
+
     }
+
+
 }
