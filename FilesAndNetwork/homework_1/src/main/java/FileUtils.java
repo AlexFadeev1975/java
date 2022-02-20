7ca6b3816957fb4fa54599d02781d0d6d864e353
@aleksei_fadeev <alex1975>
2022-02-20 15:46:18 +0300

    homework 15.5 is done

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,28 @@
-public class FileUtils {
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
-    public static long calculateFolderSize(String path) {
-        return 0;
+public class FileUtils<i> {
+
+
+    public static long calculateFolderSize(String path) throws Exception {
+
+        try (Stream<Path> stream = Files.walk(Paths.get(path), 4)) {
+            return stream.filter(Files::isRegularFile).map(x -> x.toFile().length()).mapToLong(Long::valueOf).sum();
+        }
+    }
+
+    public static void toString(String filename, long filesSize) {
+        String[] attr = {"B", "Kb", "Mb", "Gb"};
+        for (int i = 0; i < 4; i++) {
+            if (filesSize < 1024) {
+                System.out.println("Размер папки " + filename + " " + filesSize + " " + attr[i]);
+                break;
+            } else {
+                filesSize = (long) (filesSize / 1024);
+
+            }
+        }
     }
 }
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите путь до папки: ");
+            String nextLine = scanner.nextLine();
+            try {
+                FileUtils.toString(nextLine, FileUtils.calculateFolderSize(nextLine));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
     }
 }
