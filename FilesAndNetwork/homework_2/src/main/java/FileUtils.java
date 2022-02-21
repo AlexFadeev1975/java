8514f7199da4df40c2ca17172486518c9175246a
@aleksei_fadeev <alex1975>
2022-02-21 06:45:35 +0300

    homework 15.7 is done

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
+
+        Path sourceDir = Paths.get(sourceDirectory);
+        Path destinationDir = Paths.get(destinationDirectory);
+        Stream<Path> stream = Files.walk(Paths.get(sourceDirectory));
+        stream.forEach(x -> {
+            if (x.toFile().isDirectory()) {
+                try {
+                    Files.createDirectory(destinationDir.resolve(sourceDir.relativize(x)));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    Files.copy(x, destinationDir.resolve(sourceDir.relativize(x)), StandardCopyOption.REPLACE_EXISTING);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите путь папки для копирования: ");
+            String sourceDirectory = scanner.nextLine();
+            System.out.println("Введите путь папки назначения: ");
+            String destinationDirectory = scanner.nextLine();
+
+            Path sourceDir = Paths.get(sourceDirectory);
+            if (sourceDir.toFile().exists()) {
+                try {
+                    FileUtils.copyFolder(sourceDirectory, destinationDirectory);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Такой папки не существует!");
+            }
+
+
+        }
     }
 }
