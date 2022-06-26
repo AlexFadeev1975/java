f28ce184befa49c26df7858ec4faf17adcd18034
@aleksei_fadeev <alex1975>
2022-06-26 05:46:00 +0300

    Homework 22.7 is done

--- Performance/CarNumberGenerator/src/Loader.java
+++ Performance/CarNumberGenerator/src/Loader.java
@@ -1,41 +1,78 @@
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
-public class Loader {
+
+public class Loader extends Thread {
+
+    private int startNumber = 0;
+    private int endNumber = 0;
+
+
+    public Loader(int endNumber, int startNumber) {
+        this.endNumber = endNumber;
+        this.startNumber = startNumber;
+    }
 
     public static void main(String[] args) throws Exception {
-        long start = System.currentTimeMillis();
+        final int COUNTTHREADS = 7;
+        int partNumber, startNumber = 1;
+        int endNumber;
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        partNumber = 1000 / COUNTTHREADS;
+        endNumber = partNumber;
 
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for (int number = 1; number < 1000; number++) {
-            int regionCode = 199;
-            for (char firstLetter : letters) {
-                for (char secondLetter : letters) {
-                    for (char thirdLetter : letters) {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
-                    }
-                }
-            }
+        Thread[] arrThreads = new Thread[COUNTTHREADS];
+
+        for (int i = 0; i < COUNTTHREADS; i++) {
+            arrThreads[i] = new Loader(endNumber, startNumber);
+            startNumber = endNumber + 1;
+            endNumber = endNumber + partNumber;
         }
+        for (int i = 0; i < COUNTTHREADS; i++) {
 
-        writer.flush();
-        writer.close();
-
-        System.out.println((System.currentTimeMillis() - start) + " ms");
+            arrThreads[i].start();
+        }
     }
 
     private static String padNumber(int number, int numberLength) {
         String numberStr = Integer.toString(number);
         int padSize = numberLength - numberStr.length();
-
-        for (int i = 0; i < padSize; i++) {
-            numberStr = '0' + numberStr;
+        if (padSize != 0) {
+            numberStr = (padSize == 2) ? "00" + numberStr : "0" + numberStr;
         }
 
         return numberStr;
+    }
+
+    @Override
+    public void run() {
+        long start = System.currentTimeMillis();
+        String fileName = "res/numbers" + startNumber + ".txt";
+        try (PrintWriter writer = new PrintWriter(fileName)) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+            char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+            for (int number = startNumber; number < endNumber; number++) {
+                int regionCode = 199;
+                for (char firstLetter : letters) {
+                    for (char secondLetter : letters) {
+                        for (char thirdLetter : letters) {
+                            stringBuilder.append(firstLetter);
+                            stringBuilder.append(padNumber(number, 3));
+                            stringBuilder.append(secondLetter);
+                            stringBuilder.append(thirdLetter);
+                            stringBuilder.append(padNumber(regionCode, 2));
+                            stringBuilder.append('\n');
+                        }
+                    }
+                }
+            }
+            writer.write(stringBuilder.toString());
+            writer.flush();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 }
